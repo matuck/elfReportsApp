d697fca88d5b281f3d455411768eb865435852a8
Mitch Tuck <matuck@matuck.com>
2014-11-25 10:24:34 -0500

    fixes for forclose on android

---
+++ platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/home/matuck/Documents/projects/ElfReportsApp/platforms/android/ant-build/CordovaApp-release-unsigned.apk : \
+/home/matuck/Documents/projects/ElfReportsApp/platforms/android/ant-build/CordovaApp.ap_ \
+/home/matuck/Documents/projects/ElfReportsApp/platforms/android/ant-build/classes.dex \
+/home/matuck/Documents/projects/ElfReportsApp/platforms/android/CordovaLib/ant-build/classes.jar \
--- platforms/android/ant-build/ElfReports-release-unsigned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/matuck/Documents/projects/ElfReportsApp/platforms/android/ant-build/ElfReports-release-unsigned.apk : \
-/home/matuck/Documents/projects/ElfReportsApp/platforms/android/ant-build/ElfReports.ap_ \
-/home/matuck/Documents/projects/ElfReportsApp/platforms/android/ant-build/classes.dex \
-/home/matuck/Documents/projects/ElfReportsApp/platforms/android/CordovaLib/ant-build/classes.jar \
