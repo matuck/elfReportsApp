399b4726422493b42cc3c0c7f28238fe293e9951
Mitch Tuck <matuck@matuck.com>
2014-12-01 20:38:31 -0500

    pushing to production

--- platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
+++ platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -2,3 +2,5 @@
 /home/matuck/Documents/projects/ElfReportsApp/platforms/android/ant-build/CordovaApp.ap_ \
 /home/matuck/Documents/projects/ElfReportsApp/platforms/android/ant-build/classes.dex \
 /home/matuck/Documents/projects/ElfReportsApp/platforms/android/CordovaLib/ant-build/classes.jar \
+/home/matuck/Documents/projects/ElfReportsApp/platforms/android/libs/google-play-services.jar \
+/home/matuck/Documents/projects/ElfReportsApp/platforms/android/libs/cordova-generic-ad.jar \
